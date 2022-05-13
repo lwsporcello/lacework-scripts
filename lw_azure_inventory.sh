2f5e2f94b121029f6747f45bc4fdfe3fd174e46e
Steve <79470244+lwsporcello@users.noreply.github.com>
2022-05-13 13:14:30 -0400

    Update lw_azure_inventory.sh

--- lw_azure_inventory.sh
+++ lw_azure_inventory.sh
@@ -6,9 +6,20 @@
 
 # Set the initial counts to zero.
 AZURE_VMS=0
+AZURE_VMSS=0
+AKS_NODES=0
 SQL_SERVERS=0
 LOAD_BALANCERS=0
 GATEWAYS=0
+
+function getSubscriptions {
+  az account list | jq -r '.[] | .id'
+}
+
+function setSubscription {
+  SUB=$1
+  az account set --subscription $SUB
+}
 
 function getResourceGroups {
   az group list | jq -r '.[] | .name'
@@ -16,6 +27,14 @@ function getResourceGroups {
 
 function getVMs {
   az vm list -d --query "[?powerState=='VM running']" | jq length
+}
+
+function getVMSS {
+  az vmss list  --query "[].sku.capacity" | jq add
+}
+
+function getAKSNodes {
+  az aks list --query "[].agentPoolProfiles" | jq '[.[] | .[] | .count] | add'
 }
 
 function getSQLServers {
@@ -31,31 +50,57 @@ function getGateways {
   az network vnet-gateway list --resource-group $RG | jq length
 }
 
+function getSubscriptions {
+  az account list | jq -r '.[] | .id'
+}
+
+originalsub=$(az account show | jq -r '.id')
+
 echo "Starting inventory check."
-echo "Fetching VMs..."
-vms=$(getVMs)
-AZURE_VMS=$(($AZURE_VMS + $vms))
+echo "Fetching Subscriptions..."
 
-echo "Fetching SQL Databases..."
-sql=$(getSQLServers)
-SQL_SERVERS=$(($SQL_SERVERS + $sql))
+for sub in $(getSubscriptions); do
+  echo "Switching to subscription $sub"
+  setSubscription $sub
 
-echo "Fetching Load Balancers..."
-lbs=$(getLoadBalancers)
-LOAD_BALANCERS=$(($LOAD_BALANCERS + $lbs))
+  echo "Fetching VMs..."
+  vms=$(getVMs)
+  AZURE_VMS=$(($AZURE_VMS + $vms))
 
-echo "Fetching Gateways..."
-for group in $(getResourceGroups); do
-  gw=$(getGateways $group)
-  GATEWAYS=$(($GATEWAYS + $gw))
+  echo "Fetching VM Scale Sets..."
+  vmss=$(getVMSS)
+  AZURE_VMSS=$(($AZURE_VMSS + $vmss))
+  
+  echo "Fetching AKS Nodes..."
+  aks_nodes=$(getAKSNodes)
+  AKS_NODES=$(($AKS_NODES + $aks_nodes))
+
+  echo "Fetching SQL Databases..."
+  sql=$(getSQLServers)
+  SQL_SERVERS=$(($SQL_SERVERS + $sql))
+
+  echo "Fetching Load Balancers..."
+  lbs=$(getLoadBalancers)
+  LOAD_BALANCERS=$(($LOAD_BALANCERS + $lbs))
+
+  echo "Fetching Gateways..."
+  for group in $(getResourceGroups); do
+    gw=$(getGateways $group)
+    GATEWAYS=$(($GATEWAYS + $gw))
+  done
 done
+
+echo "Setting back original subscription into AZ CLI context"
+az account set --subscription $originalsub
 
 echo "######################################################################"
 echo "Lacework inventory collection complete."
 echo ""
 echo "Azure VMs:         $AZURE_VMS"
+echo "Azure VMSS:        $AZURE_VMSS"
+echo "AKS Nodes:         $AKS_NODES"
 echo "SQL Servers:       $SQL_SERVERS"
 echo "Load Balancers:    $LOAD_BALANCERS"
 echo "Vnet Gateways:     $GATEWAYS"
 echo "===================="
-echo "Total Resources:   $(($AZURE_VMS + $SQL_SERVERS + $LOAD_BALANCERS + $GATEWAYS))"
+echo "Total Resources:   $(($AZURE_VMS + $AZURE_VMSS + $SQL_SERVERS + $LOAD_BALANCERS + $GATEWAYS))"
